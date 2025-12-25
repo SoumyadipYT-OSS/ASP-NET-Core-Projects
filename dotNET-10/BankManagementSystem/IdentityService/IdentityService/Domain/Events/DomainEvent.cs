88c28b468b5ad3a122c3952458dbb832a8c54fcc
SoumyadipYT-OSS <SoumyadipYT-OSS@users.noreply.github.com>
2025-12-25 15:27:08 +0530

    Existing project is deleted

--- dotNET-10/BankManagementSystem/IdentityService/IdentityService/Domain/Events/DomainEvent.cs
+++
@@ -1,8 +0,0 @@
-namespace IdentityService.Domain.Events;
-
-public abstract class DomainEvent
-{
-    public Guid EventId { get; } = Guid.NewGuid();
-    public DateTime OccurredAt { get; } = DateTime.UtcNow;
-    public Guid AggregateId { get; protected set; }
-}
